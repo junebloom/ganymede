b8562aaad7cf06e46e0e8e2a8d92028db10d7d0b
sambrosia <samuelcwoodruff@gmail.com>
2017-09-11 15:48:34 -0600

    Add submit handling

--- client/src/components/AddSubscription/AddSubscription.js
+++ client/src/components/AddSubscription/AddSubscription.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react'
 import './AddSubscription.css'
 
 class App extends Component {
-  state = {
-    url: ''
-  }
+  state = { url: '' }
 
   handleChange = e => this.setState({ url: e.target.value })
+
+  submit = e => {
+    if (e.key && e.key !== 'Enter') return
+    console.log(this.state.url)
+    this.setState({ url: '' })
+  }
 
   render () {
     return (
       <div className="AddSubscription">
-        <input type="text" value={this.state.url} onChange={this.handleChange} placeholder="Enter an RSS/Atom feed url"/>
-        <button>Subscribe</button>
+        <input type="text"
+          value={this.state.url}
+          onKeyDown={this.submit}
+          onChange={this.handleChange}
+          placeholder="Enter an RSS/Atom feed url"
+        />
+        <button onClick={this.submit}>Subscribe</button>
       </div>
     )
   }
