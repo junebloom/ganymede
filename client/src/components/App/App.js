4dca619c1eef0b21e601ff2e39ff8fdf33781061
sambrosia <samuelcwoodruff@gmail.com>
2017-09-15 13:36:06 -0600

    Change html background color to light gray

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
+import Header from '../Header/Header'
 import Player from '../Player/Player'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
+        <Header/>
         <Player/>
       </div>
     )
