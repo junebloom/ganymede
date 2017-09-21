50e4b1609d5a31913af709f4cd007cfdc870dea6
sambrosia <samuelcwoodruff@gmail.com>
2017-09-21 17:46:13 -0600

    Fix AddSubscription layout on mobile

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -13,27 +13,44 @@ class Header extends Component {
   render = () => (
     <nav className='Header level is-mobile'>
       <div className='level-left'>
-        <span className='icon is-large'>
+        <span className={
+          `icon is-large ${
+            this.state.isAddExpanded
+              ? 'is-hidden-mobile'
+              : ''
+          }`
+        }>
           <Icon name='rocket' size='3x'/>
         </span>
+
         <h1 className='is-size-2 has-text-weight-bold is-hidden-mobile'>
           Ganymede
         </h1>
       </div>
-      <div className='level-right field is-grouped'>
-        <div className='control'>
-          <AddSubscription
-            expanded={this.state.isAddExpanded}
-            setExpanded={this.setAddExpanded}
-          />
-        </div>
-        <div className='control'>
-          <a className='button is-primary'>
-            <span className='icon'>
-              <Icon name='user-circle'/>
-            </span>
-            <p>Sign in</p>
-          </a>
+
+      <div className='level-right'>
+        <div className='field is-grouped'>
+          <div className='control'>
+            <AddSubscription
+              expanded={this.state.isAddExpanded}
+              setExpanded={this.setAddExpanded}
+            />
+          </div>
+
+          <div className={
+            `control ${
+              this.state.isAddExpanded
+                ? 'is-hidden-mobile'
+                : ''
+            }`
+          }>
+            <a className='button is-primary'>
+              <span className='icon'>
+                <Icon name='user-circle'/>
+              </span>
+              <p>Sign in</p>
+            </a>
+          </div>
         </div>
       </div>
     </nav>
