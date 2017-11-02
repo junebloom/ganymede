4bc9b2945864930e93c7504bad5dca873ab92e18
sambrosia <samuelcwoodruff@gmail.com>
2017-11-02 15:09:20 -0600

    Remove subscriptions page temporarily
    It's not doing anything, and will change significantly in the near
    future.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,35 +8,6 @@ import {
 import './App.css'
 import Header from '../Header/Header'
 import Player from '../Player/Player'
-import TileList from '../TileList/TileList'
-
-const podcasts = [
-  {
-    imgSrc:
-      'http://static1.squarespace.com/static/52d66949e4b0a8cec3bcdd46/t/52ebf67fe4b0f4af2a4502d8/1391195777839/1500w/Hello+Internet.003.png'
-  },
-  {
-    imgSrc:
-      'http://static.libsyn.com/p/assets/b/c/4/e/bc4ecd5620a58e47/logo-1400-1400.png'
-  },
-  {
-    imgSrc: 'https://api.adorable.io/avatars/480/a33.png'
-  },
-  {
-    imgSrc: 'https://api.adorable.io/avatars/480/b33.png'
-  },
-  {
-    imgSrc: 'https://api.adorable.io/avatars/480/c33.png'
-  },
-  {
-    imgSrc: 'https://api.adorable.io/avatars/480/d33.png'
-  },
-  {
-    imgSrc: 'https://api.adorable.io/avatars/480/e33.png'
-  }
-]
-
-const Subscriptions = () => <TileList items={podcasts} />
 
 const App = () => (
   <Router>
@@ -45,7 +16,7 @@ const App = () => (
 
       <main>
         <Switch>
-          <Route exact path="/" render={Subscriptions} />
+          <Route exact path="/" render={() => <div />} />
           <Redirect to="/" />
         </Switch>
       </main>
