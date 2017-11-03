2576c16e2035ee46b0822d8c772ac92fc53f850e
sambrosia <samuelcwoodruff@gmail.com>
2017-11-02 20:38:14 -0600

    Improve Login component design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Icon from '../Icon/Icon'
 import './Login.css'
 
 class Login extends Component {
   render = () => (
-    <div className="Login box">
-      <h4 className="title is-4">Passwordless Login</h4>
-      <h6 className="subtitle is-6">
-        <a href="#">How does this work?</a>
-      </h6>
+    <div className="Login">
+      <h4 className="title is-4">
+        <span>Login</span>
+        <Icon name="lock" />
+      </h4>
+      <Link to="/about-passwordless" className="is-6">
+        What is passwordless login?
+      </Link>
 
       <div className="field has-addons">
         <div className="control has-icons-left is-expanded">
-          <input className="input" type="email" placeholder="email" />
+          <input className="input" type="email" placeholder="Email" />
           <span className="icon is-left">
             <Icon name="envelope" />
           </span>
         </div>
         <div className="control">
-          <a className="button is-primary">
-            <Icon name="user-circle" />
-            <p>Log in</p>
-          </a>
+          <button className="button is-primary">Log in</button>
         </div>
       </div>
+
+      <p className="has-text-grey-light">
+        Your account is automagically created upon first login.
+      </p>
     </div>
   )
 }
