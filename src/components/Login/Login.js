e1cd875a3ff447c0cc707406db81d1ea10c7948d
sambrosia <samuelcwoodruff@gmail.com>
2017-11-05 10:57:22 -0700

    Add login form feedback for errors and success

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,64 +4,99 @@ import Icon from '../Icon/Icon'
 import './Login.css'
 
 class Login extends Component {
-  state = { email: '', sending: false }
+  state = {
+    email: '',
+    status: 'ready',
+    message: 'Your account is automagically created upon first login.'
+  }
 
   handleChange = event => {
     this.setState({ email: event.target.value })
   }
 
-  requestLogin = async () => {
-    this.setState({ sending: true })
-    await fetch(`http://localhost:4000/loginlink?email=${this.state.email}`, {
-      method: 'post'
-    })
-    this.setState({ sending: false })
+  requestLogin = async event => {
+    event.preventDefault()
+    this.setState({ status: 'sending' })
+
+    let res = null
+    try {
+      res = await fetch(
+        `http://localhost:4000/loginlink?email=${this.state.email}`,
+        { method: 'post' }
+      )
+    } catch (error) {
+      this.setState({
+        status: 'error',
+        message: `Oh no, could not contact the login server.`
+      })
+      return
+    }
+
+    if (res.ok)
+      this.setState({
+        status: 'success',
+        message: 'Please check your inbox for your authentication link.'
+      })
+    else
+      this.setState({
+        status: 'error',
+        message: `Oh no, there was a problem: ${res.status} ${res.statusText}`
+      })
   }
 
-  render = () => (
-    <div className="Login">
-      <h4 className="title is-4">
-        <span>Login</span>
-        <Icon name="lock" />
-      </h4>
-      <Link to="/about-passwordless" className="is-6">
-        What is passwordless login?
-      </Link>
+  render = () => {
+    let messageClassName = 'has-text-grey-light'
 
-      <div className="field has-addons">
-        <div className="control has-icons-left is-expanded">
-          <input
-            className="input"
-            type="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            disabled={this.state.sending}
-          />
+    if (this.state.status === 'success')
+      messageClassName = 'has-text-weight-bold'
+    else if (this.state.status === 'error')
+      messageClassName = 'has-text-danger has-text-weight-bold'
 
-          <span className="icon is-left">
-            <Icon name="envelope" />
-          </span>
-        </div>
+    return (
+      <div className="Login">
+        <h4 className="title is-4">
+          <span>Login</span>
+          <Icon name="lock" />
+        </h4>
+        <Link to="/about-passwordless">What is passwordless login?</Link>
 
-        <div className="control">
-          <button
-            className={`button is-primary${this.state.sending
-              ? ' is-loading'
-              : ''}`}
-            onClick={this.requestLogin}
-            disabled={this.state.sending}
-          >
-            Log in
-          </button>
-        </div>
+        {this.state.status === 'success' ? null : (
+          <form>
+            <div className="field has-addons">
+              <div className="control has-icons-left is-expanded">
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  disabled={this.state.status === 'sending'}
+                />
+
+                <span className="icon is-left">
+                  <Icon name="envelope" />
+                </span>
+              </div>
+
+              <div className="control">
+                <button
+                  className={`button is-primary${this.state.status === 'sending'
+                    ? ' is-loading'
+                    : ''}`}
+                  onClick={this.requestLogin}
+                  disabled={this.state.status === 'sending'}
+                >
+                  Log in
+                </button>
+              </div>
+            </div>
+          </form>
+        )}
+
+        <p className={messageClassName}>{this.state.message}</p>
       </div>
-
-      <p className="has-text-grey-light">
-        Your account is automagically created upon first login.
-      </p>
-    </div>
-  )
+    )
+  }
 }
 
 export default Login
