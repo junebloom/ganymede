e7f6b275a310548a18fa3c53f87d2cd7c4bf9881
sambrosia <samuelcwoodruff@gmail.com>
2017-11-06 14:11:06 -0700

    Fix missing props in PlayerControls test

--- src/components/Player/PlayerControls/PlayerControls.test.js
+++ src/components/Player/PlayerControls/PlayerControls.test.js
@@ -6,6 +6,16 @@ it('renders without crashing', () => {
   const errorSpy = jest.spyOn(console, 'error')
   const div = document.createElement('div')
 
-  ReactDOM.render(<PlayerControls />, div)
+  ReactDOM.render(
+    <PlayerControls
+      title="foo"
+      duration={1000}
+      time={31}
+      paused={false}
+      setTime={() => {}}
+      togglePlayback={() => {}}
+    />,
+    div
+  )
   expect(errorSpy).not.toHaveBeenCalled()
 })
