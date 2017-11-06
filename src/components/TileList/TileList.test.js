4dc940ec6b5d4fa2edeb135c4a81ecf29f358b74
sambrosia <samuelcwoodruff@gmail.com>
2017-11-06 14:03:12 -0700

    Fix props in TileList test

--- src/components/TileList/TileList.test.js
+++ src/components/TileList/TileList.test.js
@@ -6,6 +6,9 @@ it('renders without crashing', () => {
   const errorSpy = jest.spyOn(console, 'error')
   const div = document.createElement('div')
 
-  ReactDOM.render(<TileList items={[{ imgSrc: '', title: 'meow' }]} />, div)
+  ReactDOM.render(
+    <TileList items={[{ image: './cat.png', title: 'meow' }]} />,
+    div
+  )
   expect(errorSpy).not.toHaveBeenCalled()
 })
