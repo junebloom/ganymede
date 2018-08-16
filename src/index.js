b30a8b2e0fe6ed047842a98248e2ccc0d9dc3e8a
sambrosia <samuelcwoodruff@gmail.com>
2018-08-15 20:18:25 -0700

    Add firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase'
 import 'font-awesome/css/font-awesome.css'
 import 'bulma/css/bulma.css'
 import App from './components/App/App'
 import registerServiceWorker from './registerServiceWorker'
 
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyCBrQBrjFn10qnYMlbyhmDQdFb-m-OJDPY',
+  authDomain: 'ganymede-c2dce.firebaseapp.com',
+  databaseURL: 'https://ganymede-c2dce.firebaseio.com',
+  projectId: 'ganymede-c2dce',
+  storageBucket: 'ganymede-c2dce.appspot.com',
+  messagingSenderId: '99814253255'
+}
+
+firebase.initializeApp(config)
+
 ReactDOM.render(<App />, document.getElementById('root'))
+
 registerServiceWorker()
